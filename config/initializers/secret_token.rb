6c58f22a4116a31af9414f4c3de628e78f2bc889
Prateek Shinde <pshinde@live.com>
2012-12-02 16:30:40 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DempApp::Application.config.secret_token = 'af17a2e265a3249d7612530114f232195d23e069810222a1c3f23885c41bb7fa68d37a70c6f269a4d14f0e8a20d4a81ceb90b4ad64866bfcb37cb83464257e23'
